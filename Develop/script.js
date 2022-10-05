a6b396037d1c574a4d219c739525ed6cc64cb69d
Pasha Fomin <pashafomin@Pashas-MacBook-Pro.local>
2022-10-05 16:51:36 -0700

    defined more variables/added functions/ solved the web app

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,27 @@
 // Assignment code here
 
+var passwordArea = document.getElementById('password')
+
 var characters = {
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
   lowercase: 'abcdefghijklmnopqrstuvwxyz',
   numbers: '0123456789',
-  special: " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  special: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
 }
+
+function uppercase() {
+  return characters.uppercase[Math.floor(Math.random() * characters.uppercase.length)];
+}
+function lowercase() {
+  return characters.lowercase[Math.floor(Math.random() * characters.lowercase.length)];
+}
+function numbers() {
+  return characters.numbers[Math.floor(Math.random() * characters.numbers.length)];
+}
+function special() {
+  return characters.special[Math.floor(Math.random() * characters.special.length)];
+}
+
 
 
 // Get references to the #generate element
@@ -13,8 +29,11 @@ var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
 
+
 // Write password to the #password input
 function writePassword() {
+  var passwordString = ''
+
   var characterAmount = window.prompt('Enter an amount of characters:')
 
   if (characterAmount < 8 || characterAmount > 128) {
@@ -30,17 +49,45 @@ function writePassword() {
 
   var specialVerify = window.confirm('Would you like your password to contain SPECIAL characters?')
 
-  if (uppercaseVerify === true && lowercaseVerify === true && numbers === true && specialVerify === true) {
+  if (uppercaseVerify === false && lowercaseVerify === false && numbersVerify === false && specialVerify === false) {
+    window.alert('Please confirm one of the given options to generate a password!')
+  }
+
+  if (uppercaseVerify === true && lowercaseVerify === true && numbersVerify === true && specialVerify === true) {
     
   }
 
+  if (uppercaseVerify === true) {
+    passwordString = passwordString.concat(characters.uppercase)
+  }
+
+  if (lowercaseVerify === true) {
+    passwordString = passwordString.concat(characters.lowercase)
+  }
+  
+  if (numbersVerify === true) {
+    passwordString = passwordString.concat(characters.numbers)
+  } 
+
+  if (specialVerify === true) {
+    passwordString = passwordString.concat(characters.special)
+  } 
 
 
+  var passwordOptions = passwordString.split('')
 
-  var password = generatePassword();
+  var password = []
+
   
 
-  passwordText.value = password;
+  
+  for (var index = 0; index < characterAmount; index++) {
+    var randomNumber = Math.floor(Math.random() * passwordOptions.length)
+    var random = passwordOptions[randomNumber]
+    password.push(random)  
+  }
+  password = password.join('')
+  passwordArea.textContent = password
 
 }
 
